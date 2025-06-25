6eba4acd7c1f215589e906f87454242dc5684240
shivxnshxrma <shivanshs773@gmail.com>
2025-06-25 14:48:06 +0530

    updated code 25/6/25

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const messageSchema = z.object({
   content: z
     .string()
+    .trim()
     .min(10, { message: "Content must be of atleast 10 characters" })
     .max(300, { message: "Content must be of atmost 300 characters" }),
 });
